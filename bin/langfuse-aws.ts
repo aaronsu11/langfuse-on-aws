32fd3d6247c030851869cf1dd7e9d666fd9d2c2f
Aaron Su <supaaron@amazon.com>
2024-08-21 17:12:22 +1000

    chore: add documents

--- bin/langfuse-aws.ts
+++ bin/langfuse-aws.ts
@@ -15,7 +15,7 @@ new LangfuseStack(app, 'LangfuseStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '<account-id>', region: '<region-id>' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
